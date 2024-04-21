add643e7e5d0fa3e9865516127371073149b1026
BfB-Schenefeld <161332046+BfB-Schenefeld@users.noreply.github.com>
2024-04-21 18:54:35 +0200

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -26,25 +26,28 @@
 require 'open-uri'
 require 'nokogiri'
 
+def scrape_event_details(event_url)
+  puts "Accessing event page: #{event_url}"
+  document = Nokogiri::HTML(open(event_url))
+
+  # Assuming the date is in a specific format or contained in a specific element
+  # This part needs to be adjusted based on actual HTML structure of the event page
+  date_text = document.at_css('specific_selector_for_date').text.strip
+  puts "Date found on event page: #{date_text}"
+end
+
 def scrape_calendar_data(year, month)
   url = "https://www.sitzungsdienst-schenefeld.de/bi/si010_r.asp?MM=#{month}&YY=#{year}"
   puts "Attempting to access URL: #{url}"
   begin
-    document = Nokogiri::HTML(open(url))  # Use open directly as per Ruby 2.0.0
-    puts "Page loaded successfully."
-    links_found = document.css('a[href*="si010_r.asp?DD="]')
-    puts "Number of matching links found: #{links_found.count}"
+    document = Nokogiri::HTML(open(url))
+    puts "Calendar page loaded successfully."
 
-    if links_found.empty?
-      puts "No links matching the criteria were found."
-    else
-      links_found.each do |link|
-        day = link['href'][/DD=(\d+)/, 1]
-        month = link['href'][/MM=(\d+)/, 1]
-        year = link['href'][/YY=(\d+)/, 1]
-        formatted_date = "#{day}.#{month}.#{year}"
-        puts "Datum: #{formatted_date}, URL: #{link['href']}"
-      end
+    event_links = document.css('a[href*="to010_r.asp?SILFDNR="]').map { |link| "https://www.sitzungsdienst-schenefeld.de/bi/#{link['href']}" }
+    puts "Number of event links found: #{event_links.count}"
+
+    event_links.each do |link|
+      scrape_event_details(link)
     end
   rescue StandardError => e
     puts "Error during calendar data scrape: #{e.message}"
