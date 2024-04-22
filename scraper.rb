1a8a2b6b22523e87476edb728c7c706f690fc35a
BfB-Schenefeld <161332046+BfB-Schenefeld@users.noreply.github.com>
2024-04-22 18:05:02 +0200

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -29,17 +29,23 @@ require 'date'
 
 # Methode zur Extraktion und Formatierung des Datums
 def extract_and_format_date(dow, dom, month, year)
-  date = Date.parse("#{dow}, #{dom} #{Date::MONTHNAMES[month.to_i]} #{year}")
-  formatted_date = date.strftime("%a., %d.%m.%Y")
-rescue ArgumentError
-  'Invalid date'
+  # Erstellen eines Datumsstrings im deutschen Format "Tag, DD.MM.YYYY"
+  formatted_date = "#{dow}, #{dom.rjust(2, '0')}.#{month.rjust(2, '0')}.#{year}"
+  begin
+    # Parsen des Datums im deutschen Format und Konvertieren in ein Datum-Objekt
+    date = Date.strptime(formatted_date, "%a, %d.%m.%Y")
+    # Rückgabe des formatierten Datums
+    date.strftime("%a., %d.%m.%Y") # Z.B. "Di., 05.03.2024"
+  rescue ArgumentError
+    'Invalid date'
+  end
 end
 
 # Methode zum Scrapen der Kalenderdaten (Ebene 1)
 def scrape_calendar_data(year, month)
   url = "https://www.sitzungsdienst-schenefeld.de/bi/si010_r.asp?MM=#{month}&YY=#{year}"
   puts "Zugriff auf Kalenderseite: #{url}"
-  document = Nokogiri::HTML(URI.open(url))
+  document = Nokogiri::HTML(open(url))
 
   # Extraktion der Sitzungsdaten aus der Kalendertabelle
   document.css('tr:not(.emptyRow)').each do |row|
@@ -57,5 +63,6 @@ end
 
 # Testaufruf für März 2024
 scrape_calendar_data('2024', '3')
+
 
 
